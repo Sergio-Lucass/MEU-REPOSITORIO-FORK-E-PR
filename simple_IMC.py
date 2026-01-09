e12c6d7966dc2472d91e160701d69dd8f7b03017
Sergio_Lucas <sergio.rod.guapi@gmail.com>
2026-01-09 18:35:57 -0300

    Completando meus OBJETIVOS DE AULA

---
+++ simple_IMC.py
@@ -0,0 +1,9 @@
+# IMC
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+imc = peso/(altura ** 2)
+print(f"\nSeu Indice Massa Corporal é: {imc}")
+
+print("\nPara saber sua situação por favor copie o link abaixo e cole em seu navegador:")
+print("https://drpaulopittelli.com.br/como-calcular-seu-indice-de-massa-corporal-imc\n")
